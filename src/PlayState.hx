53eb9ea4d751aac092e1737f589219799a28de33
Tyler McCarthy <tweedlitweedli@gmail.com>
2020-10-24 14:09:52 -0500

    stuff in playstate for every game

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -5,6 +5,16 @@ import flixel.FlxState;
 class PlayState extends FlxState {
 	override public function create() {
 		super.create();
+
+		FlxG.mouse.visible = false;
+
+		// camera.pixelPerfectRender = true;
+		// remove vvv when going live
+		FlxG.scaleMode = new PixelPerfectScaleMode();
+
+		camera.followLerp = 0.5;
+
+		// bgColor = 0xff8b9bb4;
 	}
 
 	override public function update(elapsed:Float) {
