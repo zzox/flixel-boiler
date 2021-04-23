572c2f8b82763fa99e726357eaa7aa9c9b55cbe7
Tyler McCarthy <tweedlitweedli@gmail.com>
2021-04-23 17:47:28 -0500

    prestate

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -8,15 +8,7 @@ class PlayState extends FlxState {
     override public function create() {
         super.create();
 
-        // PROD: remove this vvv
-        // requires `-debug` flag
-        FlxG.debugger.visible = true;
-        FlxG.debugger.drawDebug = true;
-
-        FlxG.mouse.visible = false;
-
         camera.pixelPerfectRender = true;
-        FlxG.scaleMode = new PixelPerfectScaleMode();
     }
 
     override public function update(elapsed:Float) {
